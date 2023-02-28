dfa236a60d4f6ede659d305423d489351475d6bb
Hristo Yonkov <hristopturs@gmail.com>
2023-02-28 20:29:26 +0200

    updating demo

---
+++ 3 - Components Deep Dive/demo-deep-dive/src/components/Movie.js
@@ -0,0 +1,24 @@
+export default function Movie({
+    id,
+    title,
+    year,
+    plot,
+    posterUrl,
+    director,
+    onMovieDelete,
+}) {
+
+    return (
+        <article>
+            <h3>{title}, {year}</h3>
+            <main>
+                <img src={posterUrl} alt={title} />
+                <p>{plot}</p>
+            </main>
+            <footer>
+                <p>Director: {director}</p>
+                <button onClick={() => onMovieDelete(id)}>Delete</button>
+            </footer>
+        </article>
+    );
+}
---
+++ 3 - Components Deep Dive/demo-deep-dive/src/components/MovieList.js
@@ -0,0 +1,26 @@
+import { createElement } from "react";
+import Movie from "./Movie";
+
+export default function MovieList({
+    movies,
+    onMovieDelete,
+
+}) {
+    // let movieElements = [];
+    // for (const movie of movies) {
+    //     // movieElements.push(createElement(Movie, movie));
+    //     movieElements.push(<li><Movie {...movie} /></li>);
+    // }
+
+    // let movieList = movies.map(movie => <li><Movie {...movie} /></li>);
+
+    return (
+        <ul>
+            {movies.map(movie => (
+                <li key={movie.id}>
+                    <Movie {...movie} onMovieDelete={onMovieDelete} />
+                </li>
+            ))}
+        </ul>
+    );
+}
