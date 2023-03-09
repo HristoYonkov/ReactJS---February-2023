2440e3677df615774f991132905a147070bfdb63
Hristo Yonkov <hristopturs@gmail.com>
2023-03-09 13:12:14 +0200

    forms examples

---
+++ 5 - Forms/form-demo/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('Pesho');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUsername('Gosho');
+    }, 3000);
+  }, [])
+
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={onUsernameChange}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Send" />
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
