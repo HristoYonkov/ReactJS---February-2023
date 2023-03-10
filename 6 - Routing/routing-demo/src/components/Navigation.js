11df905482f3a82172f45d68f9ca6e296986bbe7
Hristo Yonkov <hristopturs@gmail.com>
2023-03-10 21:07:16 +0200

    routing example updated

---
+++ 6 - Routing/routing-demo/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import styles from './Navigation.module.css'
+import { Link } from 'react-router-dom'
+export const Navigation = () => {
+    return (
+        <nav className={styles.navigation}>
+            <ul>
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/about"}>About</Link></li>
+            </ul>
+        </nav>
+    )
+}
