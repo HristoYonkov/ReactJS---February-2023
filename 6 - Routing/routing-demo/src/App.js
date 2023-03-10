19958b5024003ab2f819fa07b3cb31ba67b8b5d5
Hristo Yonkov <hristopturs@gmail.com>
2023-03-10 20:34:07 +0200

    routing demo initialzed

---
+++ 6 - Routing/routing-demo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
