ea4d55da9d3441f5899e9d81426fb70ddb20f4bd
Hristo Yonkov <hristopturs@gmail.com>
2023-03-16 16:54:42 +0200

    updated demo todoList

---
+++ 7 - React Hooks/hooks-demo/src/components/TodoList.js
@@ -0,0 +1,16 @@
+import { ListGroup } from 'react-bootstrap';
+import { TodoItem } from './TodoItem';
+
+export const TodoList = ({
+    todos,
+}) => {
+
+    return (
+        <div style={{ width: '30%', margin: '10px auto' }}>
+            <h1>Todo List</h1>
+            <ListGroup>
+                {todos.map(x => <TodoItem key={x._id} {...x} />)}                
+            </ListGroup>
+        </div>
+    );
+}
